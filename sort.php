617c2a9c66819ca945377757fd384de99ffc9991
Dennis <73579956+dnsannt@users.noreply.github.com>
2022-04-03 21:43:39 +0700

    Update sort.php

--- sort.php
+++ sort.php
@@ -8,16 +8,6 @@
 <body>
 <h1>Sorting</h1>	
 <h3>
-	<?php
-$nilai = array(1, 3, 9, 2, 11, 4, 5);
-sort($nilai);
-
-$data = count($nilai);
-for($x = 0; $x < $data; $x++) {
-  echo $nilai[$x];
-  echo "<tr> ";
-}
-?>
 </h3>
 </body>
 </html>
